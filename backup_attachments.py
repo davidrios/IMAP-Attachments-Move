2ef6252269e9c18cdd94ce565c64750fc15b4ef3
David Rios <david.rios.gomes@gmail.com>
2016-05-01 01:34:44 -0300

    fix base64

--- backup_attachments.py
+++ backup_attachments.py
@@ -73,7 +73,7 @@ def add_saved_notice(message, destination_name, bkp_identifier):
 
         if 'base64' in content_encoding:
             message_text = base64.b64decode(message.get_payload())
-            message.set_payload(base64.b64encode(notice + message_text).decode('ascii'))
+            message.set_payload(base64.b64encode(notice.encode('ascii') + message_text).decode('ascii'))
         elif 'quoted-printable' in content_encoding:
             message.set_payload(quopri.encodestring(notice.encode('ascii')).decode('ascii') + message.get_payload())
         else:
